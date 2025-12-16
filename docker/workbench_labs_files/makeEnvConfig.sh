8f4fbb3c283c438fa838114c8c114b2d203dfa7a
Eric Monk <eric.monk@neo4j.com>
2025-12-16 08:50:53 -0500

    Fixed issues with docker build

--- docker/workbench_labs_files/makeEnvConfig.sh
+++ docker/workbench_labs_files/makeEnvConfig.sh
@@ -2,8 +2,12 @@
 echo "Creating env-config.js file"
 # environment variables should be set in envConfig.sh   
 
-sed 's|${WORKBENCH_PROTOCOL}|'$WORKBENCH_PROTOCOL'|g' $PWD/cw-config/cw-ui-template/env-config.js.template > $PWD/cw-config/cw-ui/env-config.js.1
-sed 's|${WORKBENCH_HOST}|'$WORKBENCH_HOST'|g' $PWD/cw-config/cw-ui/env-config.js.1 > $PWD/cw-config/cw-ui/env-config.js.2
-sed 's|${WORKBENCH_PORT}|'$WORKBENCH_PORT_EXTERNAL'|g' $PWD/cw-config/cw-ui/env-config.js.2 > $PWD/cw-config/cw-ui/env-config.js
+UI_TEMPLATE_DIR=$PWD/cw-config/cw-ui-template
+UI_CONFIG_DIR=$PWD/cw-config/cw-ui
+
+mkdir -p $UI_CONFIG_DIR
+sed 's|${WORKBENCH_PROTOCOL}|'$WORKBENCH_PROTOCOL'|g' $UI_TEMPLATE_DIR/env-config.js.template > $UI_CONFIG_DIR/env-config.js.1
+sed 's|${WORKBENCH_HOST}|'$WORKBENCH_HOST'|g' $UI_CONFIG_DIR/env-config.js.1 > $UI_CONFIG_DIR/env-config.js.2
+sed 's|${WORKBENCH_PORT}|'$WORKBENCH_PORT_EXTERNAL'|g' $UI_CONFIG_DIR/env-config.js.2 > $UI_CONFIG_DIR/env-config.js
 rm $PWD/cw-config/cw-ui/env-config.js.1
 rm $PWD/cw-config/cw-ui/env-config.js.2
